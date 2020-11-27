4a36f7f3fad310f4ba9f0472c7349cd91e996dcd
Nathaniel <nathaniel.ollie.smith@gmail.com>
2020-11-27 22:59:47 +0000

    fixed propigator on Layer side

--- AI_Framework(NO-DLL)/Layer.cpp
+++ AI_Framework(NO-DLL)/Layer.cpp
@@ -2,13 +2,12 @@
 
 	Neural_Network::Layer::Layer(int prevW, int width) : prevW(prevW), width(width), weight(new float[width * prevW]), bias(new float[width])
 	{
-		std::cout << static_cast <float> (rand()) / static_cast <float> (RAND_MAX) << std::endl;
 		for (int x = 0; x < width; x++)
 		{
-			bias[x] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+			bias[x] = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX)) - 0.5f;
 			for (int y = 0; y < prevW; y++)
 			{
-				weight[x * prevW + y] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+				weight[x * prevW + y] = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX)) - 0.5f;
 			}
 		}
 	}
@@ -17,31 +16,21 @@
 	{
 		delete[] weight;
 		delete[] bias;
-		std::cout << "Layer Deleted" << std::endl;
 	}
 
 	float* Neural_Network::Layer::Propigate(float* input)
 	{
 		float* ret = new float[width];
-		memcpy(ret, bias, width);
-
-		std::cout << "Layer input:" << std::endl;
-		for (int i = 0; i < prevW; i++)
-			std::cout << "\t" << input[i] << std::endl;
-
+		memcpy(ret, bias, width * sizeof(float));
 		for (int x = 0; x < width; x++)
 		{
 			for (int y = 0; y < prevW; y++)
 			{
-				ret[x] += input[y] * weight[x * prevW + y];
+				ret[x] += input[y] * weight[x * prevW + y];;
 			}
 			ret[x] = Activate(ret[x]);
 		}
 		delete[] input;
-
-		std::cout << "Layer outputs:" << std::endl;
-		for (int i = 0; i < width; i++)
-			std::cout << "\t" << ret[i] << std::endl;
 
 		return ret;
 	}
--- AI_Framework(NO-DLL)/Neural_Network.cpp
+++ AI_Framework(NO-DLL)/Neural_Network.cpp
@@ -3,30 +3,20 @@
 Neural_Network::Neural_Network(std::vector<int>& structure) : structure(structure), layers((Layer*)malloc(sizeof(Layer) * (structure.size() - 1)))
 {
 	for (size_t i = 0; i < structure.size() - 1; i++)
-	{
-		std::cout << "layer " << i << " created\n";
 		new(&layers[i]) Layer(structure[i], structure[i + 1]);
-	}
 };
 
 Neural_Network::~Neural_Network()
 {
-
 	for (size_t i = 0; i < structure.size() - 1; i++)
 		layers[i].~Layer();
 	free(layers);
-	std::cout << "Network Deleted" << std::endl;
 }
 
 std::vector<float> Neural_Network::Propigate(std::vector<float>& input)
 {
-
 	float* buffer = new float[input.size()];
-	std::copy(input.begin(), input.end(), buffer);
-
-	std::cout << "network input:" << std::endl;
-	for (int i = 0; i < structure[0]; i++)
-		std::cout << "\t" << buffer[i] << std::endl;
+	memcpy(buffer, &input[0], structure[0] * sizeof(float));
 
 	for (size_t i = 0; i < structure.size() - 1; i++)
 		buffer = layers[i].Propigate(buffer);
